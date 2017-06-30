94a2187a8c94e33a82c35b4007d0c485095ddefa
Eliza Weisman <eliza@buoyant.io>
2017-06-30 13:39:34 -0500

    Simple CLI arg parsing

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,17 @@
 extern crate flossy;
+#[macro_use] extern crate clap;
+
 fn main () {
-  unimplemented!()
+    let args = ClapApp::new(crate_name!())
+      .version(crate_version!())
+      .about(crate_description!())
+      .arg(Arg::with_name("PROXY_URL")
+              .required(true)
+              .index(1)
+              .help("URL of the proxy to test."))
+      .arg(Arg::with_name("v")
+              .short("v")
+              .multiple(true)
+              .help("Sets the level of verbosity"))
+      .get_matches();
 }
