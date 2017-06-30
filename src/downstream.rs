7d13022c57d7b55b294ef69e94624c84ab6f4ed0
Eliza Weisman <eliza@buoyant.io>
2017-06-30 15:05:14 -0500

    Start on Downstream server with tokio-minihttp

--- src/downstream.rs
+++ src/downstream.rs
@@ -1,3 +1,17 @@
+use futures::future;
+use tokio_minihttp::{Request, Response, Http};
 use tokio_service::Service;
+use std::io;
 
 pub struct Downstream;
+
+impl Service for Downstream {
+    type Request = Request;
+    type Response = Response;
+    type Error = io::Error;
+    type Future = future::Ok<Response, io::Error>;
+
+    fn call(&self, _request: Request) -> Self::Future {
+        unimplemented!()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,11 @@ extern crate futures;
 extern crate tokio_io;
 extern crate tokio_proto;
 extern crate tokio_service;
+extern crate tokio_minihttp;
 
 pub mod upstream;
 pub mod downstream;
+
+pub struct Test {
+
+}
