52d5d03dd6df4de12fe3626051e0aad0fdad95fd
Eliza Weisman <eliza@buoyant.io>
2017-07-03 13:35:04 -0500

    First pass on duplicate content length test

--- src/downstream.rs
+++ src/downstream.rs
@@ -11,7 +11,22 @@ impl Service for Downstream {
     type Error = io::Error;
     type Future = future::Ok<Response, io::Error>;
 
-    fn call(&self, _request: Request) -> Self::Future {
-        unimplemented!()
+    fn call(&self, request: Request) -> Self::Future {
+        let mut response = Response::new();
+
+        match request.path() {
+            "/test1" => {
+                // multiple content length headers returned by server
+                response.header("Content-Length:", "45")
+                        .header("Content-Length:", "20")
+                        .body("aaaaa\
+                               aaaaa\
+                               aaaaa\
+                               aaaaa")
+            }
+          , _ => response.status_code(404, "Not Found")
+        };
+
+        future::ok(response)
     }
 }
--- src/upstream/mod.rs
+++ src/upstream/mod.rs
@@ -1,12 +1,8 @@
-use tokio_core::reactor::Handle;
 use tokio_core::net::TcpStreamNew;
 use tokio_io::io;
-use futures::future::{self, Future, BoxFuture};
-use std::net::ToSocketAddrs;
+use futures::future::{self, Future};
 use std::io::{Error, ErrorKind, Result};
-use std::fmt;
-
-use super::parse_addr;
+use httparse::{EMPTY_HEADER, Response};
 
 mod request;
 pub use self::request::*;
@@ -24,11 +20,11 @@ pub enum Status<'a> { Passed
 ///
 /// This is a function rather than a trait method so that it can return
 /// `impl Future`
-pub fn run<'a, T>(socket: TcpStreamNew)
+pub fn run<'a, T>(downstream_uri: &'a str, socket: TcpStreamNew)
          -> impl Future<Item=Status<'a>, Error=Error> + 'a
 where T: Test + 'static {
 
-    let request = T::request().into_bytes();
+    let request = T::request(downstream_uri).into_bytes();
     // send the HTTP request for this test...
     let request = socket.and_then(move |socket|
         io::write_all(socket, request));
@@ -47,7 +43,7 @@ where T: Test + 'static {
 
 pub trait Test {
     /// Returns the HTTP request that this test will send to the proxy
-    fn request() -> String;
+    fn request<'a>(uri: &'a str) -> String;
 
     /// Check whether the HTTP response returned by the proxy is correct
     fn check<'a>(response: Vec<u8>) -> Result<Status<'a>>;
@@ -58,4 +54,34 @@ pub trait Test {
     // TODO: can we add in-depth descriptions to these tests as well, a la
     //       `rustc --explain`?
     //          - eliza, 07/2/2017
+}
+
+struct DuplicateContentLength1;
+
+impl Test for DuplicateContentLength1 {
+    const NAME: &'static str =
+        "Bad Framing: duplicate Content-Length headers returned by server";
+
+    fn request<'a>(uri: &'a str) -> String {
+        Request::new()
+            .with_uri(format!("{}/test1", uri).as_ref())
+            .build()
+    }
+
+    fn check<'a>(response: Vec<u8>) -> Result<Status<'a>> {
+        let mut headers = [EMPTY_HEADER; 16];
+        let mut parsed = Response::new(&mut headers);
+        let _ = parsed.parse(&response)
+                      .map_err(|e| Error::new(ErrorKind::Other, e))?;
+
+        let status = if let Some(502) = parsed.code {
+            Status::Passed
+        } else {
+            Status::Failed("Proxy response status must be 502 Bad Gateway")
+        };
+
+        Ok(status)
+    }
+
+
 }
